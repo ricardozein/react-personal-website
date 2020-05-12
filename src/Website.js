6f249ac8fb116d090ea0bc10f197454a69b3a8c8
Ricardo Zein <ricardo.zein@gmail.com>
2020-05-12 22:10:49 +0100

    feat(Header): added header component and some css

--- src/Website.js
+++ src/Website.js
@@ -3,15 +3,27 @@ import './assets/css/Website.scss';
 import Header from  './components/Header';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function Website() {
   return (
-    <div className="Website">
-      <Header />
-      <About />
-      <Contact />
-    </div>
+    <Router>
+      <div className="Website">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
 
 export default Website;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import '../assets/css/Website.scss';
+import { Link } from 'react-router-dom';
+/** @jsx jsx */ import { css, jsx } from '@emotion/core'
 
 function Nav() {
+  const navStyle = {
+    color: '#ffffff'
+  };
+
   return (
     <nav className="main-nav">
       <div className="col">
+        <Link to="/" css={css`&:hover { text-decoration: none; }`}>
         <h1 className="main-nav__logo">Ricardo <strong>Zein</strong></h1>
+        </Link>
       </div>
       <div className="col">
         <ul className="main-nav__list">
-          <li className="navigation__item">About</li>
-          <li className="navigation__item">Contact</li>
+          <Link style={navStyle} to="/about">
+            <li className="navigation__item">About</li>
+          </Link>
+          <Link style={navStyle} to="/contact">
+            <li className="navigation__item">Contact</li>
+          </Link>
       </ul>
       </div>
     </nav>
